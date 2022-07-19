f1a5605dacbddd7e42c6aa5e793a0f9124bd7949
Austin Johnson <austin@austinjohnson.me>
2022-07-19 08:16:23 -0500

    basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,17 @@ import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div className="text-2xl text-sky-400">Hello world!</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is rounder?</div>
+      <div className="p-2"></div>
+      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+        <div className="w-16 h-16 bg-red-200 "></div>
+        <div className="p-8">vs</div>
+        <div className="w-16 h-16 bg-red-200"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
